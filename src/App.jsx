bf74d2160463d88d89ce906d1718a5d58b63216a
lanbow93 <the.doctor1501@gmail.com>
2024-05-02 13:51:56 -0500

    prettier configured for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-import { Outlet } from 'react-router-dom'
+import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,17 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import App from "./App";
-import { Home } from "./pages/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import App from './App';
+import { Home } from './pages/Home';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App/>}>
-            <Route path="" element={<Home/>}/>
-        </Route>
-    )
-)
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+    </Route>
+  )
+);
 
-export default router
+export default router;
